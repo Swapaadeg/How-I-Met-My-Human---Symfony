7c397308332089516fd99295a45f4b7fcd17651d
Marie Rivier <marie.rivier23@gmail.com>
2025-11-20 09:44:36 +0100

    Add a new statistics section displaying total animals, associations count and satisfaction rate with icons and responsive design.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AnimalsRepository;
+use App\Repository\AssociationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,13 +11,17 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AnimalsRepository $animalsRepository): Response
+    public function index(AnimalsRepository $animalsRepository, AssociationRepository $associationRepository): Response
     {
         $oldestAnimals = $animalsRepository->findOldestInShelter(10);
+        $totalAnimals = $animalsRepository->count([]);
+        $totalAssociations = $associationRepository->count([]);
         
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
             'oldest_animals' => $oldestAnimals,
+            'total_animals' => $totalAnimals,
+            'total_associations' => $totalAssociations,
         ]);
     }
 }
